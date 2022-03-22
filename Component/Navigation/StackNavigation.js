49bf46598d71ea508cc31021011b1fa66cba0d0c
feheed <fmjuwaihel@gmail.com>
2022-03-22 11:50:32 +0300

    done

--- Component/Navigation/StackNavigation.js
+++ Component/Navigation/StackNavigation.js
@@ -4,14 +4,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "../Auth";
 import SignUp from "../Auth/SignUp";
 import MyEvent from "../Event/MyEvent";
+import CreateEventModal from "../Event/CreateEventModal";
 
 const StackNavigation = () => {
   const { Navigator, Screen } = createStackNavigator();
   return (
-    <Navigator initialRouteName="myevent">
+    <Navigator initialRouteName="create">
       <Screen name="signup" component={SignUp} />
       <Screen name="signin" component={SignIn} />
       <Screen name="myevent" component={MyEvent} />
+      <Screen name="create" component={CreateEventModal} />
     </Navigator>
   );
 };
