131dde4643b9a79bf80eb09a26d6c09b7f14566c
snn93 <snn93@icloud.com>
2022-03-24 09:16:35 +0300

    solve conflicts

--- Stores/api.js
+++ Stores/api.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import axios from "axios";
-//fahad's ip's
-//export const baseURL = "http://192.168.150.70:8000"; //workplace wifi
-//export const baseURL = "http://localhost:8000"; //#localhost
-export const baseURL = "http://192.168.8.180:8001"; //myStc wifi
-=======
+
 import axios from "axios"; //#localhost
 
 // Sara's ip's
@@ -14,7 +8,6 @@ export const baseURL = "http://192.168.8.188:8001"; //workplace//
 //export const baseURL = "http://192.168.150.70:8001"; //workplace wifi
 //export const baseURL = "http://localhost:8001"; //#localhost
 // export const baseURL = "http://192.168.8.180:8001"; //myStc wifi
->>>>>>> CAT
 
 const api = axios.create({
   baseURL: `${baseURL}/api`,
